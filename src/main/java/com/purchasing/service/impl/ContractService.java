2f58acda2ca3c5688390c7dc692c1fbe101da84a
Vanessa Rossi <vanessa_rossi@outlook.com>
2015-02-16 08:48:40 -0200

    Iniciando classe service

--- src/main/java/com/purchasing/service/impl/ContractService.java
+++ src/main/java/com/purchasing/service/impl/ContractService.java
@@ -1,7 +1,19 @@
 package com.purchasing.service.impl;
 
+import com.purchasing.entity.Contract;
+
+import java.io.File;
+import java.util.List;
+
 /**
  * @author vanessa
  */
 public interface ContractService {
+
+    public Contract save(Contract contract);
+    public void delete(Contract contract);
+    public File download(Contract contract);
+    public Contract searchById(Contract contract);
+    public List<Object[]> findPagination(String sSearch, int iDisplayStart, int iDisplayLength);
+    public Integer totalPagination(String sSearch);
 }
