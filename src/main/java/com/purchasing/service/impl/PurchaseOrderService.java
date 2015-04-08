ced04ef46471a84973341835f5d66cec49669ab4
Vanessa Rossi <vanessa_rossi@outlook.com>
2015-04-08 16:44:09 -0300

    Inicio de salvar uma ordem de compra

--- src/main/java/com/purchasing/service/impl/PurchaseOrderService.java
+++ src/main/java/com/purchasing/service/impl/PurchaseOrderService.java
@@ -1,6 +1,7 @@
 package com.purchasing.service.impl;
 
 import com.purchasing.entity.PurchaseOrder;
+import com.purchasing.entity.Supplier;
 
 import java.util.List;
 
@@ -11,5 +12,10 @@ public interface PurchaseOrderService {
 
     public PurchaseOrder singleSave(PurchaseOrder purchaseOrder);
     public List<PurchaseOrder> variousSave(PurchaseOrder purchaseOrder);
+
+    public List<PurchaseOrder> findBySupplierOrderDate(Supplier supplier);
+
+    public List<Object[]> findPagination(String sSearch, int iDisplayStart, int iDisplayLength);
+    public Integer totalPagination(String sSearch);
 
 }
